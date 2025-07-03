bcb305c198fdd3894ebc09fa1de3f6b7a34aee51
GabrielTavares2305 <gabrieeltavares@live.com>
2025-07-03 20:59:22 -0300

    primeira alteração

--- app/route.py
+++ app/route.py
@@ -1,7 +1,12 @@
 from app import app
-from flask import render_template
+from flask import render_template, url_for
+
+@app.route('/')
+def homepage():
+    return 'Home'
+
+
 
 @app.route('/login')
-
 def loginpage():
     return render_template('index.html')
